27e76b721e08f33b25e914c9e0b6993e8a0d15a0
Pablo Martin Tedesco Rordiguez <tedesco8@gmail.com>
2024-02-12 00:12:08 -0300

    TopBar icons are refactored and LogoutIcon is added

--- dashboard-v4/src/dashboard/TopBar.svelte
+++ dashboard-v4/src/dashboard/TopBar.svelte
@@ -1,5 +1,20 @@
 <script>
 	import { openSidebar } from './store.ts';
+
+	import CampaignIcon from '../components/docs/icons/CampaignIcon.svelte';
+	import PlusIcon from '../components/docs/icons/PlusIcon.svelte';
+	import ShareIcon from '../components/docs/icons/ShareIcon.svelte';
+	import SearchIcon from '../components/docs/icons/SearchIcon.svelte';
+	import LogoutIcon from '../components/docs/icons/LogoutIcon.svelte';
+
+	const iconsOptions = [
+		{ href: '/logout', component: LogoutIcon },
+		{ href: '/', component: ShareIcon },
+		{ href: '/', component: PlusIcon },
+		{ href: '/', component: CampaignIcon }
+	];
+
+	const handleOnClicIcon = (href) => (window.location.href = href);
 </script>
 
 <header class="h-20 items-center relative z-10">
@@ -35,15 +50,7 @@
 							<path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
 						</svg>
 					</div>
-					<svg
-						class="absolute fill-current h-4 hidden left-0 ml-4 pointer-events-none text-gray-500 w-4 group-hover:text-gray-400 sm:block"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 20 20"
-					>
-						<path
-							d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"
-						/>
-					</svg>
+					<SearchIcon />
 					<input
 						type="text"
 						class="bg-gray-800 block leading-normal pl-10 py-1.5 pr-4 ring-opacity-90 rounded-2xl text-gray-400 w-full focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -52,54 +59,13 @@
 				</div>
 			</div>
 			<div class="flex items-center justify-end ml-5 p-1 relative w-full sm:mr-0 sm:right-auto">
-				<span class="block pr-5">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width={2}
-							d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
-						/>
-					</svg>
-				</span>
-				<span class="block pr-5">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width={2}
-							d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-						/>
-					</svg>
-				</span>
-				<span class="block pr-5 relative">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width={2}
-							d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-						/>
-					</svg>
-				</span>
+				{#each iconsOptions as { href, component }, i}
+					<span class="block pr-5 cursor-pointer">
+						<div on:click={() => handleOnClicIcon(href)}>
+							<svelte:component this={component} />
+						</div>
+					</span>
+				{/each}
 				<span class="block relative">
 					<img
 						alt="Maurice Lokumba"
